a94a386b62e141f6c7a0a43151be0a688c153621
mrtwumgh <twumstephen2006@gmail.com>
2024-10-07 06:30:47 +0000

    Implemeneted Search, Filtering and Pagination features

--- movie_review_project/settings.py
+++ movie_review_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     'reviews',
     'users',
 ]
@@ -108,6 +109,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 
--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,20 @@
 from reviews.serializers import ReviewSerializer
 from reviews.models import Review
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from reviews.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ReviewListView(generics.ListAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['movie_title', 'rating']
+    ordering_fields = ['rating', 'created_date']
+    filterset_fields = ['movie_title', 'rating']
+    ordering = ['-created_date']
+
 
 
 class ReviewCreateView(generics.CreateAPIView):
