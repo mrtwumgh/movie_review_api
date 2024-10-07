e0239168c0b4dcf3849770813ca38ca8a9c20cfa
mrtwumgh <twumstephen2006@gmail.com>
2024-10-07 07:59:38 +0000

    Integrated the TMDB API functionality

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,15 +1,32 @@
 from rest_framework import serializers
 from reviews.models import Review
+from reviews.utils import get_tmdb_movie_details
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    movie_details = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
-        fields = ['id', 'user', 'movie_title', 'review_content', 'rating', 'created_date']
+        fields = [
+            'id', 
+            'user', 
+            'movie_title', 
+            'review_content', 
+            'rating', 
+            'created_date',
+            'movie_details'
+        ]
+
+    def get_movie_details(self, obj):
+        movie_data = get_tmdb_movie_details(obj.movie_title)
+        if movie_data:
+            return movie_data
+        else:
+            return {'Error': 'Movie details not found'}
 
     def validate_rating(self, data):
-        if data["rating"] < 1 or data["rating"] > 5:
+        if data < 1 or data > 5:
             raise serializers.ValidationError("Rating should be between 1 and 5")
         return data
---
+++ reviews/utils.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import tmdbsimple
+
+
+load_dotenv()
+
+tmdbsimple.API_KEY = os.getenv('TMDB_API_KEY')
+
+def get_tmdb_movie_details(title):
+    try:
+        search = tmdbsimple.Search()
+        response = search.movie(query=title)
+        if search.results:
+            movie_id = search.results[0]['id']
+            movie = tmdbsimple.Movies(movie_id)
+            movie_info = movie.info()
+            movie_details = {
+                'title': movie_info.get('title'),
+                'year': movie_info.get('release_date', '')[:4],
+                'description': movie_info.get('overview'),
+                'genre': [genre['name'] for genre in movie_info.get('genres', [])],
+                'country_of_origin': [country['name'] for country in movie_info.get('production_countries', [])],
+            }
+            return movie_details
+        else:
+            return {'Error': 'Movie not found'}
+    except Exception as e:
+        print(f"Error fetching movie details: {e}")
+        return {'Error': 'Failed to fetch movie details.'}
+
+
