43f0ebee51e957a82144bce8c1587280b4b0d16b
mrtwumgh <twumstephen2006@gmail.com>
2024-10-10 06:56:45 +0000

    set project up for deployment

--- movie_review_project/settings.py
+++ movie_review_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i5xkhy*11^cw9#kx+4^6!kmc)*dqjmp-8+*l038)&%&+1fe#79'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mrtwum.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
