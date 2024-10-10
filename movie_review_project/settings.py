6a6da516d661aa992a5cc97e377ec47dfce1b46c
mrtwumgh <twumstephen2006@gmail.com>
2024-10-10 09:15:03 +0000

    configured ALLOWED_HOSTS

--- movie_review_project/settings.py
+++ movie_review_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mrtwum.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['movie-review-api.render.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
