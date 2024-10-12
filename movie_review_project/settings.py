ac02301653c7fd4b7668874d7dba95d30ab1c9ac
mrtwumgh <twumstephen2006@gmail.com>
2024-10-12 07:17:08 +0000

    reconfigured settings

--- movie_review_project/settings.py
+++ movie_review_project/settings.py
@@ -29,9 +29,9 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == "True"
 
-ALLOWED_HOSTS = ['movie-review-api.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
